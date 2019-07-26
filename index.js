1f802948a205352ee1b1125dd8cb45c7fdd6d898
E.B <edenbenbow@yahoo.com>
2019-07-26 03:06:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   return var="John Lennon plays guitar"
+  for ([initialization]; [condition]; [iteration]) {
+    loopBody];
+  }
 
 }
