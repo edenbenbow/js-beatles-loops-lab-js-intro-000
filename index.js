92fbbe0d9c74f70ee0320deef30ba73123e95f62
E.B <edenbenbow@yahoo.com>
2019-07-26 03:17:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,12 @@ function johnLennonFacts(facts) {
 
 return results;
 }
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
+}
