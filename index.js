0291610529da600cb260b494bf32eef803dc6546
E.B <edenbenbow@yahoo.com>
2019-07-26 03:35:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 
 }
+
+function johnLennonFacts(facts) {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var newFacts = [];
+  var i = 0
+  while (i <facts.length) {
+    newFacts.push(facts[count] + "!!!");
+    i++;
+  }
+}
+
+return newFacts;
+}
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
+}
