4cc937c416107b176b000140e96651a647061945
E.B <edenbenbow@yahoo.com>
2019-07-26 03:11:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var array = []
-  for (let i = 0; i < 25; i++) {
+  for (var i = 0; i < 25; i++) {
     loopBody];
   }
   console.log("John Lennon plays guitar")
