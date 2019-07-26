be7b7bc424111b5ece1c3f5f403685f3a5af2fa4
E.B <edenbenbow@yahoo.com>
2019-07-26 03:35:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + "plays" + instruments[i]);
+    array.push(musicians[i] + " plays " + instruments[i]);
   }
   return array;
 
